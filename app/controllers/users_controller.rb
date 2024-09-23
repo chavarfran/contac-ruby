8c1a6b7990aa9aed7a8176116ec62129a1cd49c9
Francisco Chavarria <fchavarriar@outlook.com>
2024-09-23 12:55:09 -0600

    🎨 Code > structure code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Conctacto registrado satisfactoriamente"
+      flash[:success] = "Contacto registrado satisfactoriamente"
       redirect_to root_url
     else
       render "new"
@@ -29,8 +29,7 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,13 +16,11 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :password, length: { maximum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :email, presence: true, length: { maximum: 255 },
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: true }
                                     has_many :contacts
-
-
   private
   def email_downcase
     self.email.downcase!
